10efa3af9435f381af115ab48cadc67c94e5cbad
m0baxter <m0baxter@gmail.com>
2026-01-16 14:48:02 -0500

    Moved most training arguments into configuration file.

---
+++ gobots/utils/parse_config.py
@@ -0,0 +1,140 @@
+import yaml
+from enum import Enum
+from pydantic import BaseModel
+from typing import Any
+
+
+class LRScheduleType(str, Enum):
+    linear = "linear"
+    cosine = "cosine"
+    cosine_with_restarts = "cosine_with_restarts"
+    polynomial = "polynomial"
+    constant = "constant"
+    constant_with_warmup = "constant_with_warmup"
+    inverse_sqrt = "inverse_sqrt"
+    reduce_lr_on_plateau = "reduce_lr_on_plateau"
+    cosine_with_min_lr = "cosine_with_min_lr"
+    cosine_warmup_with_min_lr = "cosine_warmup_with_min_lr"
+    warmup_stable_decay = "warmup_stable_decay"
+
+
+class LogLevel(str, Enum):
+    debug = "debug"
+    info = "info"
+    warning = "warning"
+    error = "error"
+    critical = "critical"
+    passive = "passive"
+
+
+class LoggingStrategy(str, Enum):
+    no = "no"
+    epoch = "epoch"
+    steps = "steps"
+
+
+class SaveStrategy(str, Enum):
+    no = "no"
+    epoch = "epoch"
+    steps = "steps"
+    best = "best"
+
+
+class Optimizer(str, Enum):
+    adamw_torch = "adamw_torch"
+    adamw_torch_fused = "adamw_torch_fused"
+    adamw_torch_xla = "adamw_torch_xla"
+    adamw_torch_npu_fused = "adamw_torch_npu_fused"
+    adamw_apex_fused = "adamw_apex_fused"
+    adafactor = "adafactor"
+    adamw_anyprecision = "adamw_anyprecision"
+    adamw_torch_4bit = "adamw_torch_4bit"
+    adamw_torch_8bit = "adamw_torch_8bit"
+    ademamix = "ademamix"
+    sgd = "sgd"
+    adagrad = "adagrad"
+    adamw_bnb_8bit = "adamw_bnb_8bit"
+    adamw_8bit = "adamw_8bit"
+    ademamix_8bit = "ademamix_8bit"
+    lion_8bit = "lion_8bit"
+    lion_32bit = "lion_32bit"
+    paged_adamw_32bit = "paged_adamw_32bit"
+    paged_adamw_8bit = "paged_adamw_8bit"
+    paged_ademamix_32bit = "paged_ademamix_32bit"
+    paged_ademamix_8bit = "paged_ademamix_8bit"
+    paged_lion_32bit = "paged_lion_32bit"
+    paged_lion_8bit = "paged_lion_8bit"
+    rmsprop = "rmsprop"
+    rmsprop_bnb = "rmsprop_bnb"
+    rmsprop_bnb_8bit = "rmsprop_bnb_8bit"
+    rmsprop_bnb_32bit = "rmsprop_bnb_32bit"
+    galore_adamw = "galore_adamw"
+    galore_adamw_8bit = "galore_adamw_8bit"
+    galore_adafactor = "galore_adafactor"
+    galore_adamw_layerwise = "galore_adamw_layerwise"
+    galore_adamw_8bit_layerwise = "galore_adamw_8bit_layerwise"
+    galore_adafactor_layerwise = "galore_adafactor_layerwise"
+    lomo = "lomo"
+    adalomo = "adalomo"
+    grokadamw = "grokadamw"
+    schedule_free_radam = "schedule_free_radam"
+    schedule_free_adamw = "schedule_free_adamw"
+    schedule_free_sgd = "schedule_free_sgd"
+    apollo_adamw = "apollo_adamw"
+    apollo_adamw_layerwise = "apollo_adamw_layerwise"
+    stable_adamw = "stable_adamw"
+
+
+class ZClipMode(str, Enum):
+    zscore = "zscore"
+    percentile = "percentile"
+
+
+class ClipOption(str, Enum):
+    adaptive_scaling = "adaptive_scaling"
+    mean = "mean"
+
+
+class ZClipConfig(BaseModel):
+    mode: ZClipMode = ZClipMode.zscore
+    alpha: float = 0.97
+    clip_option: ClipOption = ClipOption.adaptive_scaling
+    z_thresh: float = 2.5
+    clip_factor: float = 1.0
+    max_grad_norm: float = 1.0
+    warmup_steps: int = 25
+
+
+class TrainingConfig(BaseModel):
+    run_name: str
+    max_steps: int
+    warmup_steps: int
+    lr_scheduler_type: LRScheduleType
+    lr_scheduler_kwargs: dict[str, Any]
+    use_bf16: bool = True
+    use_tf32: bool = True
+    log_level: LogLevel
+    logging_steps: int
+    output_dir: str
+    per_device_train_batch_size: int
+    gradient_accumulation_steps: int
+    save_strategy: SaveStrategy
+    save_steps: int
+    save_total_limit: int
+    logging_strategy: LoggingStrategy
+    optim: Optimizer
+    learning_rate: float
+    max_grad_norm: float
+    weight_decay: float
+    auxiliary_loss_weight: float
+    z_loss_weight: float
+    mtp_weight: float
+    zclip_config: ZClipConfig
+    context_length: int = 2048
+
+
+def load_config_file(path):
+    with open(path, "r") as stream:
+        config_dict = yaml.safe_load(stream)
+
+    return TrainingConfig.validate(config_dict["training"])
--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 from multiprocessing import cpu_count
 from pathlib import Path
@@ -6,6 +7,7 @@ from gobots.callbacks import ZClipCallback
 from gobots.model_factory import build_bagl_hybrid
 from gobots.trainers import MTPTrainer, LossAccumulator
 from gobots.utils.data_utils import prepare_pretraining_datasets
+from gobots.utils.parse_config import load_config_file
 
 torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
@@ -16,7 +18,16 @@ torch.backends.cudnn.rnn.fp32_precision = "tf32"
 torch.set_float32_matmul_precision("high")
 
 
+def create_argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--training_config", choices=["train", "eval"])
+
+    return parser
+
+
 if __name__ == "__main__":
+    args = create_argparser().parse_args()
+    training_config = load_config_file(args.training_config)
     num_shards = int(cpu_count() * 0.75)
     loss_accumlator = LossAccumulator()
 
@@ -51,41 +62,38 @@ if __name__ == "__main__":
 
     train_dataset = prepare_pretraining_datasets(
         tokenizer,
-        context_length=2048 + 1 + config.mtp_config.num_nextn_predict_layers,
+        context_length=training_config.context_length
+        + 1
+        + config.mtp_config.num_nextn_predict_layers,
         num_shards=num_shards,
     )
 
     training_args = TrainingArguments(
         disable_tqdm=True,
-        max_steps=92000,
-        warmup_steps=2000,
-        lr_scheduler_type="warmup_stable_decay",
-        lr_scheduler_kwargs={
-            "num_decay_steps": 9200,
-            "decay_type": "linear",
-        },
-        per_device_train_batch_size=10,
-        gradient_accumulation_steps=4,
-        per_device_eval_batch_size=2,
+        max_steps=training_config.max_steps,
+        warmup_steps=training_config.warmup_steps,
+        lr_scheduler_type=training_config.lr_scheduler_type,
+        lr_scheduler_kwargs=training_config.lr_scheduler_kwargs,
+        per_device_train_batch_size=training_config.per_device_train_batch_size,
+        gradient_accumulation_steps=training_config.gradient_accumulation_steps,
         eval_strategy="no",
-        save_strategy="steps",
-        save_steps=5000,
-        save_total_limit=5,
-        logging_strategy="steps",
-        logging_steps=1,
-        output_dir="./output_dir",
-        log_level="info",
-        bf16=True,
-        tf32=True,
-        optim="adamw_torch_fused",
-        learning_rate=1.5e-03,
+        save_strategy=training_config.save_strategy,
+        save_steps=training_config.save_steps,
+        save_total_limit=training_config.save_total_limit,
+        logging_strategy=training_config.logging_strategy,
+        logging_steps=training_config.logging_steps,
+        output_dir=training_config.output_dir,
+        log_level=training_config.log_level,
+        bf16=training_config.use_bf16,
+        tf32=training_config.use_tf32,
+        optim=training_config.optim,
+        learning_rate=training_config.learning_rate,
         dataloader_num_workers=num_shards,
         dataloader_prefetch_factor=8,
-        batch_eval_metrics=True,
         include_num_input_tokens_seen=True,
-        max_grad_norm=10.0,
-        weight_decay=0.01,
-        run_name="bagl_hybrid_08",
+        max_grad_norm=training_config.max_grad_norm,
+        weight_decay=training_config.weight_decay,
+        run_name=training_config.run_name,
         report_to="trackio",
         trackio_space_id=None,
         dataloader_drop_last=True,
@@ -94,25 +102,25 @@ if __name__ == "__main__":
         model=model,
         args=training_args,
         train_dataset=train_dataset,
-        auxiliary_loss_weight=1e-4,
-        z_loss_weight=0.001,
-        mtp_weight=0.01,
+        auxiliary_loss_weight=training_config.auxiliary_loss_weight,
+        z_loss_weight=training_config.z_loss_weight,
+        mtp_weight=training_config.mtp_weight,
         mtp_depth=config.mtp_config.num_nextn_predict_layers,
         compute_metrics=compute_metrics,
         callbacks=[
             ZClipCallback(
-                mode="zscore",
-                alpha=0.97,
-                clip_option="adaptive_scaling",
-                z_thresh=2.5,
-                clip_factor=0.95,
-                max_grad_norm=1.0,
-                warmup_steps=25,
+                mode=training_config.zclip_config,
+                alpha=training_config.zclip_config,
+                clip_option=training_config.zclip_config,
+                z_thresh=training_config.zclip_config,
+                clip_factor=training_config.zclip_config,
+                max_grad_norm=training_config.zclip_config,
+                warmup_steps=training_config.zclip_config,
             )
         ],
     )
 
-    if any(Path("./output_dir").iterdir()):
+    if any(Path(training_config.output_dir).iterdir()):
         trainer.train(resume_from_checkpoint=True)
 
     else:
@@ -121,7 +129,7 @@ if __name__ == "__main__":
     trainer.accelerator.wait_for_everyone()
     trainer.accelerator.state.fsdp_plugin.set_state_dict_type("FULL_STATE_DICT")
 
-    tokenizer.save_pretrained("./output_dir")
-    trainer.save_model("./output_dir")
+    tokenizer.save_pretrained(training_config.output_dir)
+    trainer.save_model(training_config.output_dir)
 
     trainer.accelerator.end_training()
